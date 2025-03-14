c9e1fee27dd595f4c65cd2fe7482c50904467342
aaroncano <aaroncanoc1@gmail.com>
2025-03-13 23:21:11 -0600

    contact

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,29 +1,27 @@
 import {
   Title,
   Text,
-  Grid,
   TextInput,
   Textarea,
   Button,
   Group,
   Flex,
-  Box,
-  Mark
+  Stack
 } from '@mantine/core';
 import { IconMail, IconBrandGithub, IconMapPin } from '@tabler/icons-react';
 import classes from './Contact.module.css';
 
 export function Contact() {
   return (
-    <Box className={classes.container}>
-      <Grid className={classes.contactContainer}>
-        <Grid.Col span={{ base: 12, md: 6 }} className={classes.contactSection}>
-          <Title order={1} mb="xl"><Mark color="red">Contact Me</Mark></Title>
-          <Text size="lg" mb="xl">
-            If you want to collaborate or say hi, feel free to get in touch with me.
-          </Text>
+    <Stack className={classes.wrapper} align='center' mt="md" mb="md">
+      <div className={classes.inner}>
+          <Title className={classes.title}>Contact</Title>
 
-          <Flex direction="column" gap="xl">
+
+          <Flex direction="column" gap="xl" mt="md">
+            <Text className={classes.description}>
+              Let’s talk! I’m just one message away. 
+            </Text>
             <Group className={classes.contactItem}>
               <IconMail size={24} />
               <div>
@@ -44,58 +42,13 @@ export function Contact() {
               <IconMapPin size={24} />
               <div>
                 <Text fw={500}>Location</Text>
-                <Text c="dimmed">Mexico</Text>
+                <Text c="dimmed">Tabasco, Mexico</Text>
               </div>
             </Group>
           </Flex>
-        </Grid.Col>
 
-        <Grid.Col span={{ base: 12, md: 6 }} className={classes.contactSection}>
-          <Title order={2} className={classes.formTitle} mb="xl">
-            Send a Message
-          </Title>
 
-          <form>
-            <Flex direction="column" gap="md">
-              <TextInput
-                label="Name"
-                placeholder="Your name"
-                required
-                variant='filled'
-                radius="md"
-              />
-
-              <TextInput
-                label="Email"
-                placeholder="your.email@example.com"
-                required
-                type="email"
-                variant='filled'
-                radius="md"
-              />
-
-              <Textarea
-                label="Message"
-                placeholder="Your message..."
-                required
-                variant='filled'
-                minRows={6}
-                radius="md"
-              />
-
-              <Button
-                size="md"
-                color="green"
-                radius="md"
-                variant="light"
-                className={classes.submitButton}
-              >
-                Send
-              </Button>
-            </Flex>
-          </form>
-        </Grid.Col>
-      </Grid>
-    </Box>
+      </div>
+    </Stack>
   );
 }
